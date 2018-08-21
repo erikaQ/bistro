3e5fca0696da24684580030b56ef485e66079f66
Erika Kogan <koganbox727@gmail.com>
2018-08-21 19:43:16 +0600

    done

--- js/function.js
+++ js/function.js
@@ -15,27 +15,7 @@ $(document).ready(function() {
 		event.preventDefault();
 	});
 
-	// Scroll to ID // Плавный скролл к элементу при нажатии на ссылку. В ссылке указываем ID элемента
-	// $('#main__menu a[href^="#"]').click( function(){ 
-	// 	var scroll_el = $(this).attr('href'); 
-	// 	if ($(scroll_el).length != 0) {
-	// 	$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
-	// 	}
-	// 	return false;
-	// });
-
-	// Stiky menu // Липкое меню. При прокрутке к элементу #header добавляется класс .stiky который и стилизуем
-    // $(document).ready(function(){
-    //     var HeaderTop = $('.header').offset().top;
-        
-    //     $(window).scroll(function(){
-    //             if( $(window).scrollTop() > HeaderTop ) {
-    //                     $('.header').addClass('stiky');
-    //             } else {
-    //                     $('.header').removeClass('stiky');
-    //             }
-    //     });
-    // });
+	// Scroll to ID // Плавный скролл к элементу при нажатии на ссылку.
     $(document).ready(function(){
         var HeaderTop = $('#js-header').offset().top + 50;
         
@@ -210,54 +190,31 @@ $(document).ready(function() {
 function formSubmit() {
     $("[type=submit]").on('click', function (e){ 
         e.preventDefault();
-        // Заводим переменные
-        // Ищем родительскую фору для того чтобы манипулировать элементами находящимися только внутри неё
         var form = $(this).closest('form');
-        // Запоминаем путь к php обработчику формы
         var url = form.attr('action');
-        // Собираем все данные с полей формы для отправки
         var form_data = form.serialize();
-        // Выбираем все обязательные поля по атрибуту required
         var field = form.find('[required]');
 
-        // Задаем количество пустых полей по умолчанию
         empty = 0;
 
-        // Перебираем каждое обязательное поле
         field.each(function() {
-            // Если поля пустые
             if ($(this).val() == "") {
-                // Добавляем класс invalid
                 $(this).addClass('invalid');
-                // Увеличиваем счеткик пустых полей
                 empty++;
-            // Если поля не пустые
             } else {
-                // Убираем класс invalid
                 $(this).removeClass('invalid');
-                // Добавляем класс valid если необходимо для стилизации
                 $(this).addClass('valid');
             }  
         });
 
-        // Можно проверить пересчет пустых полей в консоли
-        // console.log(empty);
 
-        // Если пустых полей больше 0
         if (empty > 0) {
-            // Останавливаем работу скрипта запрещая отправку формы
             return false;
-        // Если пустых полей нет
-        } else {        
-            // Запускаем отправку формы без перезагрузки страницы
             $.ajax({
-                // Используем переменные в параметрах для отправки формы
                 url: url,
                 type: "POST",
                 dataType: "html",
                 data: form_data,
-                // При успешной отправке
-                // В аргумент response(произвольное название) можно записать и видеть результат ответа сервера
                 success: function (response) {
                     console.log(response);
                     // Дальше несколько вариантов
@@ -285,15 +242,4 @@ function formSubmit() {
             $(this).removeClass('invalid');
         }
     });
-    // Если есть чекбокс политикой можно отключать кнопку при снятом чекбоксе добавляя к кнопке атрибут disabled 
-    // $('.form__privacy input').on('change', function(event) {
-    //     event.preventDefault();
-    //     var btn = $(this).closest('.form').find('.btn');
-    //     if ($(this).prop('checked')) {
-    //         btn.removeAttr('disabled');
-    //         // console.log('checked');
-    //     } else {
-    //         btn.attr('disabled', true);
-    //     }
-    // });
 }
